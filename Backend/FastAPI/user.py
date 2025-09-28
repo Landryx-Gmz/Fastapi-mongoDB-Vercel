b24717c2a31e4f60c10637fb5e42b709952fd0a1
Andy Gomez <153938897+Landryx-Gmz@users.noreply.github.com>
2025-09-28 16:58:49 +0200

    API users y usersjson

--- Backend/FastAPI/user.py
+++ Backend/FastAPI/user.py
@@ -1,7 +1,31 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+# Entidad User
+class User(BaseModel):
+    name: str
+    surname: str
+    url: str
+    age: int
+
+users_list = [
+    User(name="Andy", surname="Gomez", url="https://moure.dev", age=36),
+    User(name="Rougs", surname="Mont", url="https://Rougs.com", age=35),
+    User(name="Any", surname="Gomez", url="https://Ani.es", age=27),
+]
+
+
+@app.get("/usersjson")
+async def usersjson():
+    return [
+        {"name": "Andy", "surname": "Gomez", "url": "https://moure.dev", "age": 36},
+        {"name": "Rougs", "surname": "Mont", "url": "https://Rougs.com", "age": 35},
+        {"name": "Any", "surname": "Gomez", "url": "https://Ani.es", "age": 27},
+    ]
+
+
 @app.get("/users")
 async def users():
-    return "Hola Users"
+    return users_list
