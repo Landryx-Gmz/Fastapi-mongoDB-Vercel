b346484cf4551ea261dc9f0edd75fb185dd455e4
Andy Gomez <153938897+Landryx-Gmz@users.noreply.github.com>
2025-10-01 08:49:42 +0200

    Recursos estaticos

--- Backend/FastAPI/main.py
+++ Backend/FastAPI/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 
+# Importaciones de Routers
 from routers import products
 from routers import user
+
+# Importaciones de recursos estaticos
+from fastapi.staticfiles import StaticFiles
 
 
 app = FastAPI()
@@ -9,6 +13,10 @@ app = FastAPI()
 # Routers
 app.include_router(products.router)
 app.include_router(user.router)
+
+# Recursos estaticos
+app.mount("/static",StaticFiles(directory="static"),name="static")
+# Link para verlo en el explorador: http://127.0.0.1:8000/static/images/Logo.png
 
 @app.get("/")
 async def root():
