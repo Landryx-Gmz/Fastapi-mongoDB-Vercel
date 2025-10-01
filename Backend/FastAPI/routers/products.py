1eea84efc10dc002445bc94b66b9feac05808faf
Andy Gomez <153938897+Landryx-Gmz@users.noreply.github.com>
2025-10-01 08:26:28 +0200

    APIRouter y comentarios

--- Backend/FastAPI/routers/products.py
+++ Backend/FastAPI/routers/products.py
@@ -1,7 +1,7 @@
-from fastapi import APIRouter
+from fastapi import APIRouter# en vez de importar a FastAPI, importamos APIRouter
 
-router = APIRouter(prefix="/products",# con prefix hacemos que todos los endpoint sean ("/")
-                    tags=["products"],#con tags= despues del prefix o prefijo hacemos que en la documentación de fastAPI lo agrupe todo del prefix
+router = APIRouter(prefix="/products",# con prefix hacemos que todos los endpoint sean ("/") por defecto
+                    tags=["products"],#con tags= hacemos que en la documentación de fastAPI lo agrupe todo por el prefix
                     responses={404:{"message":"No encontado"}})#Con responses definimos el codigo de error y mensaje
 
 products_list= ["producto 1","producto 2","producto 3","producto 4",]
