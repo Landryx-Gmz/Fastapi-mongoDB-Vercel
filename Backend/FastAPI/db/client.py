7029e6d9fa6c4055dd6c9c376f08b35db983573c
Andy Gomez <153938897+Landryx-Gmz@users.noreply.github.com>
2025-10-07 21:21:28 +0200

    put ok

--- Backend/FastAPI/db/client.py
+++ Backend/FastAPI/db/client.py
@@ -1,4 +1,4 @@
 #Encargado de la conexion con mongoDB
 from pymongo import MongoClient
 
-db_client = MongoClient() # entre parentesis 
+db_client = MongoClient().local # con esto el cliente siempre se conecta a nuestra base en local
--- Backend/FastAPI/routers/user_db.py
+++ Backend/FastAPI/routers/user_db.py
@@ -1,7 +1,10 @@
 from fastapi import APIRouter, HTTPException,status
 from db.models.user import User
+# Importamos la base de datos de monogdb desde client.py
 from db.client import db_client
-from db.schemas.user import user_schema
+from db.schemas.user import user_schema, users_schema
+# Importaciones para tipo de id que ustiliza mongodb
+from bson import ObjectId
 
 router = APIRouter(prefix="/userdb",
                     tags=["userdb"],
@@ -11,19 +14,19 @@ router = APIRouter(prefix="/userdb",
 
 users_list = []
 
-@router.get("/",status_code=200)
+@router.get("/",response_model=list[User],status_code=200)
 async def users():
-    return users_list
+    return users_schema(db_client.users.find())
 
 # Path
 @router.get("/{id}")
-async def user(id:int):
-    return search_user(id)
+async def user(id:str):
+    return search_user("_id", ObjectId(id))
 
 # Query
-@router.get("/",response_model=User,status_code=200)
-async def user(id:int):
-    return search_user(id)
+@router.get("/",status_code=200)
+async def user(id:str):
+    return search_user("_id",ObjectId(id))
 
 
     
@@ -32,56 +35,51 @@ async def user(id:int):
 @router.post("/",response_model=User, status_code=status.HTTP_201_CREATED)
 async def user(user: User):
 
-    if type(search_user_by_email(user.email)) == User:
+    if type(search_user("email",user.email)) == User:
         raise HTTPException(
             status_code= status.HTTP_404_NOT_FOUND, detail="Usuario ya existe")
 
     user_dict = dict(user)
     del user_dict["id"] #eliminamos el id para que mongo nos lo proporcione
 
-    id= db_client.local.users.insert_one(user_dict).inserted_id # para acceder a mongo se hace con .local
+    id= db_client.users.insert_one(user_dict).inserted_id # para acceder a mongo se hace con .local
 
-    new_user = user_schema(db_client.local.users.find_one({"_id":id}))# el nombre de campo que crea mongo es _id
+    new_user = user_schema(db_client.users.find_one({"_id":id}))# el nombre de campo que crea mongo es _id
 
     return User(**new_user)
 
 # Put
-@router.put("/",response_model=User, status_code=200)
+@router.put("/{id}",response_model=User)
 async def user(user:User):
+    user_dict = dict(user)
+    del user_dict["id"]
 
-    found = False
+    try:
+        
 
-    for index, saved_user in enumerate(users_list):
-        if saved_user.id == user.id:
-            users_list[index] = user
-            found = True
-            break
-
-    if not found:
-        raise HTTPException(status_code=404, detail="No se a actualizado el usuario")
+        db_client.users.find_one_and_replace(
+            {"_id":ObjectId(user.id)}, user_dict)
     
-    return user
+    except:
+        return {"error":"No se a actualizado el usuario"}
+
+    return search_user("_id",ObjectId(user.id))
 
 # Delete
-@router.delete("/{id}",status_code=200)
-async def user(id:int):
+@router.delete("/{id}",status_code=status.HTTP_204_NO_CONTENT)
+async def user(id:str):
 
-    found = False
-
-    for index, saved_user in enumerate(users_list):
-        if saved_user.id == id:
-            del users_list[index]
-            found = True                    
-            break
+    found = db_client.users.find_one_and_delete({"_id":ObjectId(id)})
             
     if not found:
-        raise HTTPException(status_code=404, detail=f"El usuario con ID {id} no fue encontrado.")
-    return {"message": "Usuario eliminado con éxito"}
+        return {"message": "Usuario eliminado con éxito"}
 
-def search_user_by_email(email: str):
+    
+
+def search_user(field: str, key):
     
     try:
-        user = db_client.local.users.find_one({"email": email})            
+        user = db_client.users.find_one({field: key})            
         return User(**user_schema(user))
     except:
         return {"Error!":"No se a encontrado el usuario"}
