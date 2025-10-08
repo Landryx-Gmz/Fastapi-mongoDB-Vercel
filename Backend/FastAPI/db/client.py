4645c9a3fff04427e976e83e9c920e45b511eca8
Andy Gomez <153938897+Landryx-Gmz@users.noreply.github.com>
2025-10-08 21:09:53 +0200

    concexion con mongoDB atlas ok

--- Backend/FastAPI/db/client.py
+++ Backend/FastAPI/db/client.py
@@ -1,4 +1,10 @@
 #Encargado de la conexion con mongoDB
 from pymongo import MongoClient
 
-db_client = MongoClient().local # con esto el cliente siempre se conecta a nuestra base en local
+
+# Conexion a base de datos local
+# db_client = MongoClient().local # con esto el cliente siempre se conecta a nuestra base en local
+
+# Base de Datos Remoto
+db_client = MongoClient(
+    "mongodb+srv://landryx_db:landryxBase@clusterpython.bvwn5bo.mongodb.net/?retryWrites=true&w=majority&appName=ClusterPython").landryx_db
